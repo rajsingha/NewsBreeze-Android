fd46186acea946664320e043cf9468060aea1382
rajsingha <r.singha4520@gmail.com>
2021-06-10 20:23:35 +0530

    Newsapi

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,7 +4,7 @@
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_newsbreeze_app_helpers_Konstant_getAPIKey(JNIEnv *env, jobject thiz) {
-    const std::string apiKey = "7cb9e4e7c58b43249bcc92ebfba98087";
+    const std::string apiKey = "{Your newsapi goes here}"; // put your newsapi here.
     return env->NewStringUTF(apiKey.c_str());
 }extern "C"
 JNIEXPORT jstring JNICALL
